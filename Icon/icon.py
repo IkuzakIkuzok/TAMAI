ce0df78ccf1711420e51f9dd4666ae0fa6ad876b
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2023-09-26 15:13:46 +0900

    Fixed unbalanced icons.

--- Icon/icon.py
+++ Icon/icon.py
@@ -7,7 +7,7 @@ icon_size = (256, 256)
 icon = Image.new('RGBA', icon_size, (255, 255, 255, 0))
 draw = ImageDraw.Draw(icon)
 
-draw.rectangle(((16., 32.), (224., 224.)), fill='white')
+draw.rectangle(((16., 32.), (239., 224.)), fill='white')
 
 wave_colors = [(255, 16, 16, 255), (16, 255, 16, 255), (16, 16, 255, 255)]
 wave_amplitudes = [64, 16, 32]
@@ -20,7 +20,7 @@ for i in range(len(wave_colors)):
     wave_frequency = wave_frequencies[i]
     previous_point = None
 
-    for x in range(17, 224):
+    for x in range(17, 239):
         y = int(icon_size[1] / 2 + wave_amplitude * math.sin(2 * math.pi * wave_frequency * x / 196))
 
         if previous_point is not None:
@@ -29,11 +29,11 @@ for i in range(len(wave_colors)):
         previous_point = (x, y)
 
 spectrum_color = (0, 0, 0, 255)
-spectrum_line = [(x + 16, int(icon_size[1] / 2)) for x in range(208)]
+spectrum_line = [(x + 16, int(icon_size[1] / 2)) for x in range(223)]
 draw.line(spectrum_line, fill=spectrum_color, width=wave_width)
 
 frame_color = (196, 196, 196, 255)
 frame_thickness = 4
-draw.rectangle(((16., 32.), (224., 224.)), outline=frame_color, width=frame_thickness)
+draw.rectangle(((16., 32.), (239., 224.)), outline=frame_color, width=frame_thickness)
 
 icon.save('Icon.png')
